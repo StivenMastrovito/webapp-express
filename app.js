383e09ed1f9c9af51c8bd4d95408e83ac5e147e6
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-01-22 17:41:36 +0100

    implementazione AI

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ import moviesRouter from "./routers/films.js";
 import reviewsRouter from "./routers/reviews.js";
 import notFound from "./middlewares/notFound.js";
 import cors from "cors";
-
+import {GoogleGenAI}  from "@google/genai";
 
 const app = express();
 const port = process.env.PORT;
+
 
 app.use(cors({
     origin: "http://localhost:5173"
@@ -16,6 +17,24 @@ app.use(cors({
 
 app.use(express.static("public"));
 app.use(express.json());
+
+// const ai = new GoogleGenAI({});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3-flash-preview",
+//     contents: "puoi scrivere in italiano?",
+//     config: {
+//       systemInstruction: "",
+//     },
+//   });
+//   console.log(response.text);
+// }
+
+
+// main();
+
+
 
 app.use("/api/movies", moviesRouter)
 app.use("/api/reviews", reviewsRouter)
@@ -25,6 +44,6 @@ app.use(errorHandler)
 app.use(notFound);
 
 app.listen(port, (err) => {
-    if(err) throw err;
-    console.log("Server is listening on port", port);  
+    if (err) throw err;
+    console.log("Server is listening on port", port);
 })
